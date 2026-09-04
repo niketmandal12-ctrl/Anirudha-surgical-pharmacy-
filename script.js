5aa18fb3b055f227cd99e2e139dd8e8583e1ea08
niketmandal12-ctrl <niketmandal12@gmail.com>
2026-09-04 19:40:24 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.nav a[href^="#"]').forEach(link=>{
+  link.addEventListener('click',()=>{
+    document.querySelectorAll('.nav a').forEach(a=>a.classList.remove('active'));
+    link.classList.add('active');
+  });
+});
